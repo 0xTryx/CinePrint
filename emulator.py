12f40951ce74ce0fc0bb27785c558fee741a4ab9
micuit-cuit <contact@micuit-cuit.com>
2026-09-08 17:07:00 +0200

    addEmulator

---
+++ emulator.py
@@ -0,0 +1,20 @@
+import socket
+
+HOST = "192.168.1.200"
+PORT = 8080
+
+def send(sock, cmd):
+    sock.sendall((cmd + "\n").encode())
+    return sock.recv(1024).decode().strip()
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+
+    # Ping
+    print(send(s, "0,ping"))
+
+    # Println
+    print(send(s, "1,test aaa 1234567890"))
+
+    # Write (ESC @ = reset imprimante thermique)
+    print(send(s, "2,2,1B;40"))
